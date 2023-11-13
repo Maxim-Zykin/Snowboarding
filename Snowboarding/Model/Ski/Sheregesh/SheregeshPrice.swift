fca46a125ec242d9de21c2221b912289414f2d76
MaximZykin <zikinmax95@mail.ru>
2023-11-13 21:30:21 +0700

    change Sheregesh price

--- Snowboarding/Model/Ski/Sheregesh/SheregeshPrice.swift
+++ Snowboarding/Model/Ski/Sheregesh/SheregeshPrice.swift
@@ -7,52 +7,37 @@
 
 import Foundation
 
-public struct SheregeshPrice {
-    let liftName: String
-    let cost: String
-}
+let singleSkiPassOneDay = AllPrice(liftName: "Единый ски-пасс 1 день", cost: "2645 ₽")
+let singleSkiPassTwoDays = AllPrice(liftName: "Единый ски-пасс 2 дня", cost: "4800 ₽")
+let singleSkiPassThreeDays = AllPrice(liftName: "Единый ски-пасс 3 дня", cost: "7000 ₽")
+let singleSkiPassFourDays = AllPrice(liftName: "Единый ски-пасс 4 дня", cost: "8850 ₽")
+let singleSkiPassFiveDays = AllPrice(liftName: "Единый ски-пасс 5 дней", cost: "11000 ₽")
 
-public struct SheregeshPriceGroup {
-  public let sky: [SheregeshPrice]
-}
+let cascadeFourHours = AllPrice(liftName: "Каскад Ски-пасс 4 часа", cost: "1725 ₽")
+let cascadeOneDay = AllPrice(liftName: "Каскад Ски-пасс 1 день", cost: "2415 ₽")
+let cascadeTwoDays = AllPrice(liftName: "Каскад Ски-пасс 2 дня", cost: "4315 ₽")
+let cascadeThreeDays = AllPrice(liftName: "Каскад Ски-пасс 3 дня", cost: "6270 ₽")
+let cascadeFourDays = AllPrice(liftName: "Каскад Ски-пасс 4 дня", cost: "7900 ₽")
+let cascadeFiveDaya = AllPrice(liftName: "Каскад Ски-пасс 5 дней", cost: "9850 ₽")
+let cascadeSixDays = AllPrice(liftName: "Каскад Ски-пасс 6 дней", cost: "11800 ₽")
+let cascsdeSevenDays = AllPrice(liftName: "Каскад Ски-пасс 7 дней", cost: "13800 ₽")
 
-extension SheregeshPriceGroup{
+let BKD_39 = AllPrice(liftName: "Каскад Бугель нижний БКД-39", cost: "100 ₽")
+let BKD_40 = AllPrice(liftName: "Каскад Бугель верхний БКД-40", cost: "100 ₽")
+let blueArmchair = AllPrice(liftName: "Каскад 2х кресло «Синее кресло»", cost: "160 ₽")
+let educational = AllPrice(liftName: "Каскад 4х кресло «Учебная»", cost: "245 ₽")
+let bun = AllPrice(liftName: "Каскад 4х кресло «Булочка»", cost: "245 ₽")
+let westC = AllPrice(liftName: "Каскад 4х кресло «Запад»", cost: "245 ₽")
+let breadbox = AllPrice(liftName: "Каскад 4х кресло «Хлебница»", cost: "310 ₽")
+let olympiaExpress = AllPrice(liftName: "Каскад 4х кресло «Олимпия-Экспресс»", cost: "310 ₽")
+let mustagh = AllPrice(liftName: "Каскад 4х кресло «Мустаг»", cost: "310 ₽")
+let eastC = AllPrice(liftName: "Каскад 6х кресло «Восток»", cost: "310 ₽")
+let travelator = AllPrice(liftName: "Каскад Траволатор", cost: "115 ₽")
 
-    public static func skiShow() -> SheregeshPriceGroup{
+let sectorESkiPassOneDay = AllPrice(liftName: "Сектор Е Ски-пасс 1 день", cost: "2250 ₽")
+let sectorESkiPassTwoDays = AllPrice(liftName: "Сектор Е Ски-пасс 2 дня", cost: "4000 ₽")
+let sectorESkiPassThreeDays = AllPrice(liftName: "Сектор Е Ски-пасс 3 дня", cost: "5800 ₽")
+let sectorESkiPassFourDays = AllPrice(liftName: "Сектор Е Ски-пасс 4 часа", cost: "1700 ₽")
 
-        let price = [
-            SheregeshPrice(liftName: "Единый ски-пасс 1 день", cost: "2645 ₽"),
-            SheregeshPrice(liftName: "Единый ски-пасс 2 дня", cost: "4800 ₽"),
-            SheregeshPrice(liftName: "Единый ски-пасс 3 дня", cost: "7000 ₽"),
-            SheregeshPrice(liftName: "Единый ски-пасс 4 дня", cost: "8850 ₽"),
-            SheregeshPrice(liftName: "Единый ски-пасс 5 дней", cost: "11000 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 4 часа", cost: "1725 ₽"),
-            
-            SheregeshPrice(liftName: "Каскад Ски-пасс 1 день", cost: "2415 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 2 дня", cost: "4315 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 3 дня", cost: "6270 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 4 дня", cost: "7900 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 5 дней", cost: "9850 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 6 дней", cost: "11800 ₽"),
-            SheregeshPrice(liftName: "Каскад Ски-пасс 7 дней", cost: "13800 ₽"),
-            
-            SheregeshPrice(liftName: "Каскад Бугель нижний БКД-39", cost: "100 ₽"),
-            SheregeshPrice(liftName: "Каскад Бугель верхний БКД-40", cost: "100 ₽"),
-            SheregeshPrice(liftName: "Каскад 2х кресло «Синее кресло»", cost: "160 ₽"),
-            SheregeshPrice(liftName: "Каскад 4х кресло «Учебная»", cost: "245 ₽"),
-            SheregeshPrice(liftName: "Каскад 4х кресло «Булочка»", cost: "245 ₽"),
-            SheregeshPrice(liftName: "Каскад 4х кресло «Запад»", cost: "245 ₽"),
-            SheregeshPrice(liftName: "Каскад 4х кресло «Хлебница»", cost: "310 ₽"),
-            SheregeshPrice(liftName: "Каскад 4х кресло «Олимпия-Экспресс»", cost: "310 ₽"),
-            SheregeshPrice(liftName: "Каскад 4х кресло «Мустаг»", cost: "310 ₽"),
-            SheregeshPrice(liftName: "Каскад 6х кресло «Восток»", cost: "310 ₽"),
-            SheregeshPrice(liftName: "Каскад Траволатор", cost: "115 ₽"),
-            
-            SheregeshPrice(liftName: "Сектор Е Ски-пасс 1 день", cost: "2250 ₽"),
-            SheregeshPrice(liftName: "Сектор Е Ски-пасс 2 дня", cost: "4000 ₽"),
-            SheregeshPrice(liftName: "Сектор Е Ски-пасс 3 дня", cost: "5800 ₽"),
-            SheregeshPrice(liftName: "Сектор Е Ски-пасс 4 часа", cost: "1700 ₽"),
-        ]
-        return SheregeshPriceGroup(sky: price)
-    }
-}
+let priceSheregesh = [singleSkiPassOneDay, singleSkiPassTwoDays, singleSkiPassThreeDays, singleSkiPassFourDays, singleSkiPassFiveDays, cascadeFourHours, cascadeOneDay, cascadeTwoDays, cascadeThreeDays, cascadeFourDays, cascadeFiveDaya, cascadeSixDays, cascsdeSevenDays, BKD_39, BKD_40, blueArmchair, educational, bun, westC, breadbox, olympiaExpress, mustagh, eastC, travelator, sectorESkiPassOneDay, sectorESkiPassTwoDays, sectorESkiPassThreeDays, sectorESkiPassFourDays]
+
