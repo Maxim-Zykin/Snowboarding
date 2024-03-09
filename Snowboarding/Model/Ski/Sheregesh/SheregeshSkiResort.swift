130acddf216507c86293f699f1fd64d60790d42f
MaximZykin <zikinmax95@mail.ru>
2024-03-09 20:04:36 +0700

    .

--- Snowboarding/Model/Ski/Sheregesh/SheregeshSkiResort.swift
+++ Snowboarding/Model/Ski/Sheregesh/SheregeshSkiResort.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+let sheregeshInfo = ModelSkiResort(
+    nameSkiResot: "ШЕРЕГЕШ",
+    allTracks: "16",
+    heightDifference: "630",
+    totalLengthOfTracks: "35",
+    apiWeather: API.apiWeatherElbrusRegion,
+    imageSkiR: "sheregeshLogo",
+    descroption: "Шерегеш находится на юге Кемеровской области, в Горной Шории — красивой и дикой гористой местности на стыке Саян, Алтая и Кузнецкого Алатау.",
+    timeWorkModel: allTimeWorkSheregesh,
+    costModel: priceSheregesh,
+    hotelModel: hotelsSheregesh,
+    rentsModel: rentsSheregeshAll,
+    latitudeMap: 52.95282,
+    longitudeMap: 87.954984)
+
+
