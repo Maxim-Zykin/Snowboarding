faadef88a427f62d38a3e73ea3e5475d06330dd0
MaximZykin <zikinmax95@mail.ru>
2024-03-10 19:52:10 +0700

    /

--- Snowboarding/Model/Ski/Arhyz/ArhyzSkiResort.swift
+++ Snowboarding/Model/Ski/Arhyz/ArhyzSkiResort.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+let arhyzInfo = ModelSkiResort(
+    nameSkiResot: "АРХЫЗ",
+    allTracks: "16",
+    heightDifference: "854",
+    totalLengthOfTracks: "26,8",
+    apiWeather: API.apiWeatherArhyz,
+    imageSkiR: "arhyz",
+    descroption: "Всесезонный курорт в горах Северного Кавказа, ориентирован на активный семейный отдых. Зимой сюда едут кататься на лыжах и сноуборде, летом доступны трассы для горного велосипеда, пешие и конные маршруты, джиппинг, экскурсии по красивейшим окрестностям Архыза.",
+    timeWorkModel: allTimeWorkArhyz,
+    costModel: priceArhyz,
+    hotelModel: hotelsArhyzAll,
+    rentsModel: rentsArhyzAll,
+    latitudeMap: 43.544585,
+    longitudeMap: 41.181382)
--- Snowboarding/Model/Ski/Dombai/DombaiSkiResort.swift
+++ Snowboarding/Model/Ski/Dombai/DombaiSkiResort.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+let dombaiInfo = ModelSkiResort(
+    nameSkiResot: "ДОМБАЙ",
+    allTracks: "9",
+    heightDifference: "1538",
+    totalLengthOfTracks: "16,2",
+    apiWeather: API.apiWeatherDombai,
+    imageSkiR: "dombai",
+    descroption: "Горный курорт Домбай (Домбайская поляна) расположился на территории Карачаево-Черкесской республики, у подножия Главного хребта Кавказских гор. Домбай относится к всесезонному туристическому комплексу: с декабря по апрель создаются идеальные условия для активного катания по склонам, май - август - время для пешего туризма, ранняя осень - “бархатный” сезон, и только октябрь и ноябрь - сезон затишья.",
+    timeWorkModel: allTimeWorkDombai,
+    costModel: allPriceDombai,
+    hotelModel: hotelsDombaiAll,
+    rentsModel: rentsDombaiAll,
+    latitudeMap: 43.290642,
+    longitudeMap: 41.624346)
